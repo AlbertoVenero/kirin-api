5c32034b24a6024a977decb63b9fc887b4f6fbeb
DanteVzla <albertovenero@gmail.com>
2025-05-17 23:45:27 -0400

    config database provider app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,22 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { ConfigModule } from './config/config.module';
 import { AuthModule } from './auth/auth.module';
-import { FuncionarioModule } from './funcionario/funcionario.module';
-import { HcbiModule } from './hcbi/hcbi.module';
+import { FuncionarioModule } from './features/funcionario/funcionario.module';
+import { HcbiModule } from './features/hcbi/hcbi.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { databaseProviders } from './config/app/database.providers';
+
+
+
 
 @Module({
-  imports: [ConfigModule, AuthModule, FuncionarioModule, HcbiModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    TypeOrmModule.forRoot(databaseProviders),
+    ConfigModule.forRoot(), 
+    AuthModule, 
+    FuncionarioModule, 
+    HcbiModule],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
